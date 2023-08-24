8a40d430ced22cefed6fb19e4b26747df7ea45c1
Sandip323 <143005508+Sandip323@users.noreply.github.com>
2023-08-24 18:20:14 +0530

    Update A.java

--- Gits/src/Giteg/A.java
+++ Gits/src/Giteg/A.java
@@ -2,14 +2,22 @@ package Giteg;
 
 public class A {
 
-	public static void main(String[] args) {
+public void m1() 
+	{		
 	 int a=10;
 	 int b=20;
-	 
-	 int c=a+b;
-	 
+	 int c=a+b; 
 	 System.out.println(c);
-
+	}
+	public void m2()
+	{
+         System.out.println("xyz");
+	}
+	public static void main(String[] args)
+	{
+	A a=new A();
+	a.m1();
+	a.m2();
 	}
 
 }
